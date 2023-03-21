c294c24305c4bee6a5c7f7cf1ffdae73ac8a3416
andriandrian <naird.id@gmail.com>
2023-03-21 23:44:58 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <!--FontAwesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+
+    <title>Nadya</title>
+</head>
+<body style="font-family: Poppins;">
+
+    <div class="container">
+        <section class="section-hero">
+            <div class="row row1">
+                <div class="col-lg-6 hero-1">
+                    <img class="profile" src={{ asset('images/1.jpg') }} alt="">
+                    <h1>Nadya Natasha</h1>
+                    <h5>Student</h5>
+                </div>
+                <div class="col-lg-6 description">
+                    <div class="row">
+                        <p class="desc-text">
+                            Hi, I'm Nadya Natasha. Currently studying at Batam International University. Majoring in Computer Science
+                        </p>
+                        <div class="contact">
+                            <h2>Contact</h2>
+                            <p>Feel free to contact me</p>
+                            <a href="https://www.instagram.com/nadyanattasha/"><i class="fab fa-instagram contact-icon"></i></a>
+                            <a href="https://www.instagram.com/nadyanattasha/"><i class="fab fa-linkedin contact-icon"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+</body>
+</html>
